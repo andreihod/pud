e6c2067165f3b064a5f75a869e34077ec8145451
Andrei <eu@andreih.com>
2020-10-19 20:33:20 -0300

    Add simple GTK sourceview UI

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,35 @@
-mod lang;
-
 #[macro_use]
 extern crate pest_derive;
 #[macro_use]
 extern crate lazy_static;
 
+mod lang;
+use gtk::prelude::*;
+
 fn main() {
-    let input = "2 + 2 * 4.0 oi 3 + 5 * 2";
-    println!("'{}' evaluates to {:#?}", input, lang::evaluate(input));
+    if gtk::init().is_err() {
+        println!("Failed to initialize GTK.");
+        return;
+    }
+
+    let glade_src = include_str!("ui.glade");
+    let builder = gtk::Builder::from_string(glade_src);
+    let window: gtk::Window = builder.get_object("main_window").unwrap();
+    let editor: sourceview::View = builder.get_object("sourceview").unwrap();
+
+    editor.get_buffer().unwrap().connect_changed(|text_buffer| {
+        let text = text_buffer
+            .get_text(
+                &text_buffer.get_start_iter(),
+                &text_buffer.get_end_iter(),
+                false,
+            )
+            .unwrap()
+            .to_string();
+        println!("{:#?}", lang::evaluate(text.as_str()));
+    });
+
+    window.show_all();
+
+    gtk::main();
 }
